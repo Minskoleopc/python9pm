845a36dc00a1b751f924bb87cf86922516a5995f
unknown <Minskoleopc>
2024-02-15 21:57:10 +0530

    added

--- script3.py
+++ script3.py
@@ -74,15 +74,108 @@ setB = {"ram","satish","sachin","ramesh"}
 
 
 #program 6
-setH = {11,22,33}
-setJ = {44,55,66}
-setK  = setH.union(setJ)
-print(setK)
+# setH = {11,22,33}
+# setJ = {44,55,66}
+# setK  = setH.union(setJ)
+# print(setK)
 
-setL = {11,22,33}
-setB = {44,55,33}
-setQ  = setL.intersection(setB)
-print(setQ)
+# setL = {11,22,33}
+# setB = {44,55,33}
+# setQ  = setL.intersection(setB)
+# print(setQ)
+
+
+# program 1
+setA = {11,22,33}
+setB = {33,44,55}
+setC = setA.union(setB)
+print(setC)
+
+# program 2
+
+setC = {11,22,33,44}
+setD = {33,44,55}
+# setE = setC.intersection(setD)
+# print(setE)
+setC.intersection_update(setD)
+print(setC)
+
+
+setL = {11,22,33,44}
+setJ = {66,77,88,99,22,44}
+setL.intersection_update(setJ)
+print(setL)
+
+
+# program 3
+
+setF = {11,22,33,44}
+setB = {66,11,33,99}
+
+#setG = setF.symmetric_difference(setB)
+setF.symmetric_difference_update(setB)
+print(setF)
+
+
+setQ = {11,22,33}
+setM = {44,55,22}
+
+# setN = setQ.symmetric_difference(setM)
+# print(setN)
+setM.symmetric_difference_update(setQ)
+print(setM)
+
+
+# program 4
+setQ1 = {11,22,44}
+setQ2 = {66,22,88}
+# setW = setQ1.difference(setQ2)
+# print(setW)
+# print(setQ1)
+
+setQ1.difference_update(setQ2)
+print(setQ1)
+
+
+# program 5
+setQ1 = {11,22,33}
+setQ2 = {11,22,33,44}
+e = setQ2.issuperset(setQ1)
+f = setQ1.issuperset(setQ2)
+print(e)
+print(f)
+x = setQ1.issubset(setQ2)
+print(x)
+
+# program6
+
+setA = {11,22,33,88}
+setB = {77,88,99}
+h = setA.isdisjoint(setB)
+print(h)
+
+setG = {11,22,33,44}
+w = setG.discard(55)
+print(w)
+print(setG)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
