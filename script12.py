62de532f2846a3cf5604c322aa27ac6edead956b
unknown <Minskoleopc>
2024-03-09 19:15:23 +0530

    added

---
+++ script12.py
@@ -0,0 +1,67 @@
+# program  1
+# class Person:
+#     # properties and methods 
+#     first_name = "chinmay"
+#     last_name = "deshpande"
+#     age = 12
+
+#     def display(self):
+#         print(self.first_name + self.last_name)
+
+# amol = Person()
+# print(amol.first_name)
+# print(amol.last_name)
+# print(amol.age)
+# amol.display()
+
+# sharddha = Person()
+# print(sharddha.first_name)
+# print(sharddha.last_name)
+# print(sharddha.age)
+# sharddha.display()
+
+# sharddha.first_name = "shraddha"
+# sharddha.last_name = "das"
+# sharddha.age = 34
+# sharddha.city = "pune"
+# print(sharddha.first_name)
+# print(sharddha.city)
+
+# program 2
+# class Person:
+#     def __init__(self,fn,ln,age):
+#         self.firstName = fn 
+#         self.lastName = ln 
+#         self.age = age
+
+#     def display(self):
+#         print(self.firstName + self.lastName)
+
+#     def displayAge(self):
+#         print(self.age)
+
+# amol = Person("amol","rao",20)
+# chinmay = Person("chinmay","deshpande",23)
+# shraddha = Person("sharddha","pote",45)
+# amol.display()
+# chinmay.display()
+# chinmay.displayAge()
+# print(chinmay.age)
+
+# students  = [amol,chinmay,shraddha]
+# for student in students:
+#     student.display()
+#     student.displayAge()
+
+# program 3 
+# class Vehicle:
+#     def __init__(self,color,type):
+#         self.color  = color 
+#         self.type = type
+
+#     def displayColor(self):
+#         print(self.color)
+
+# audi = Vehicle("red","sedane")
+# bmw = Vehicle("blue","SUV")
+# audi.displayColor()
