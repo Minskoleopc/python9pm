d6f98bc64b702f1b7762f3dfcb454c9a3f654c50
unknown <Minskoleopc>
2024-04-02 22:01:59 +0530

    added

---
+++ script23.py
@@ -0,0 +1,115 @@
+# compile time 
+
+# def add():
+# print(8+8)
+
+# run time 
+
+# program 2
+
+# x = int(input("Enter the value 1 "))
+# y = int(input("Enter the value 2 "))
+# print(x/y)
+
+# listA = [11,22,33,44]
+# print(listA[4]) 
+
+
+# program3
+# def calculateBonusPlusSalary(salary):
+#     return 0.10 * salary
+# print(calculateBonusPlusSalary(1000))
+# print("hello")
+# try:
+#     x = int(input("Enter the value 1 "))
+#     y = int(input("Enter the value 2 "))
+#     print(x/y)
+# except Exception:
+#     print('Invalid input')
+# print("bye")
+
+# program 4
+# try:
+#     names = ["poorva","sarika","abhisha"]
+#     x = int(input("Enter the value 1 "))
+#     y = int(input("Enter the value 2 "))
+#     print(x/y)
+#     print(names[2])
+#     print(a)
+
+# except ArithmeticError:
+#     print('Invalid input')
+# except IndexError:
+#     print("increase value of your list")
+# except NameError:
+#     print("not defined")
+# except Exception:
+#     print("something went wront")
+# print("bye")
+
+
+#program 5
+
+# try:
+#     names = ["poorva","sarika","abhisha"]
+#     x = int(input("Enter the value 1 "))
+#     y = int(input("Enter the value 2 "))
+#     print(x/y)
+
+# except ArithmeticError:
+#     print('Invalid input')
+
+# finally:
+#     print("I will always execute")
+
+# program 6
+
+# try:
+#     x = int(input("Enter the value 1 "))
+#     y = int(input("Enter the value 2 "))
+#     print(x/y)
+
+# except ArithmeticError:
+#     print('Invalid input')
+
+# else:
+#     print("hello")
+
+# program 7
+# print('hello')
+# try:
+#     x = int(input("Enter the value 1 "))
+#     y = int(input("Enter the value 2 "))
+#     print(x/y)
+
+# except ArithmeticError:
+#     print('Invalid input')
+# else:
+#     print("hello")
+# finally:
+#     print("i willl always... ")
+# print("bye")
+
+
+# A single try block can be follwed by several except block 
+# Multiple except blocks can be used to handle multiple excpetions 
+# We cannot write except block with try block
+# Else block and finally block are not compulsory
+# Whem there is no exception else block is executed after try block 
+# Finally block is always executed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# logical errors
